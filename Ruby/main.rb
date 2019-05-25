1705e832e135d0622861ba8a8173a88286b61f10
carlosgonzalez96 <carlosgonz817@gmail.com>
2019-05-24 18:51:51 -0700

    fixed a encoding problem when trying to read bigdata.txt

--- Ruby/main.rb
+++ Ruby/main.rb
@@ -1,18 +1,4 @@
-# files = ["file1.txt", "file2.txt", "file3.txt"]
-# def get_word_count (f)
-#     sum = 0
-#     f.each do |f|
-#         lines = File.readlines(f) #read the lines of each file in the array
-#         line_count = lines.size
-#         text = lines.join
-#         word_count = text.split.length
-#         sum += word_count
-#
-#     end
-#     puts sum
-
-# end
-files = ["file1.txt", "file2.txt", "file3.txt"]
+files = ["file1.txt", "file2.txt", "file3.txt", "bigdata.txt"]
 threads = []
 
 sum = 0
@@ -21,7 +7,7 @@ files.each do |f|
         lines = File.readlines(t) #read the lines of each file in the array
         line_count = lines.size
         text = lines.join
-        word_count = text.split.length
+        word_count = text.force_encoding("iso-8859-1").split.length
         sum += word_count
     end
 end
