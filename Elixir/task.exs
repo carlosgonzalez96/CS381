fadd9375017bf00e01d7311172f24277245a81b7
unknown <michael77commins@gmail.com>
2019-05-28 12:21:43 -0700

    Added Tasks

--- Elixir/task.exs
+++ Elixir/task.exs
@@ -1,12 +1,4 @@
-defmodule Reader do
-
-	def spawn_proc(file) do
-		spawn Reader, :send_func, [self(), file]
-	end
-
-	def send_func(pid, file) do
-		send pid, {:msg, file_words_helper(file)}
-	end
+defmodule TaskReader do
 
 	def file_words_helper(file) do
 		case File.read(file) do
@@ -21,12 +13,9 @@ defmodule Reader do
 		if x == [] do 
 			0
 		else 
-			spawn_proc(hd x)
+			task = Task.async(Reader, :file_words_helper, [hd(x)])
 			rest = file_words(tl x)
-			receive do 
-				{:msg, msg} -> msg + rest
-			end
+			Task.await(task) + rest
 		end
 	end
 end
-
