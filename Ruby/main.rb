dc27ddbcde225747960c52e131dbb09afd2b3680
carlosgonzalez96 <carlosgonz817@gmail.com>
2019-05-30 09:46:29 -0700

    added a few changes in ruby main for threading

--- Ruby/main.rb
+++ Ruby/main.rb
@@ -1,13 +1,13 @@
-files = ["file1.txt", "file2.txt", "file3.txt", "bigdata.txt"]
+files = ["bigdata.txt", "bigdata.txt", "bigdata.txt", "bigdata.txt"]
 threads = []
 
 sum = 0
 files.each do |f|
     threads << Thread.new(f) do |t|
-        lines = File.readlines(t) #read the lines of each file in the array
-        text = lines.join #join the lines
-        word_count = text.force_encoding("iso-8859-1").split.length #count each word by refrencing the space
-        sum += word_count #keep adding the words
+        lines = File.readlines(t)
+        text = lines.join
+        word_count = text.force_encoding("iso-8859-1").split.length
+        sum += word_count
     end
 end
 
